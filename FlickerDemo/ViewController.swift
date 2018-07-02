92972f8278a8d06b476f77263dc84d3c9c839e8b
Ravi Tailor <ravi.tailor@timesinternet.in>
2018-07-02 11:47:22 +0530

    Initial Commit

---
+++ FlickerDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickerDemo
+//
+//  Created by Ravi Tailor on 02/07/18.
+//  Copyright © 2018 Ravi Tailor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
